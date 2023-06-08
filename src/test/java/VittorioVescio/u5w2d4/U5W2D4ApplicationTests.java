b3311e52fe956a1d36ac58403eac4d36488670e1
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-06-08 13:48:06 +0200

    Initial commit

---
+++ src/test/java/VittorioVescio/u5w2d4/U5W2D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package VittorioVescio.u5w2d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
